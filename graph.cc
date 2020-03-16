faa1d8ccf1c431911d2f4e68033de240cea0ab2a
JX Wang <jxwang92@gmail.com>
2020-03-16 20:04:14 +0900

    Format and move the defination out

--- graph.cc
+++ graph.cc
@@ -1,8 +1,8 @@
 #include <boost/any.hpp>
-#include <boost/optional.hpp>
 #include <iostream>
 #include <memory>
 #include <stdexcept>
+#include <string>
 #include <unordered_map>
 #include <utility>
 #include <vector>
@@ -35,55 +35,63 @@ class Graph {
    public:
     Graph() {}
     ~Graph() {}
-    void init_adjlist(const K &key) {
-        if (!exist_in_map(key, adjacent_lists)) {
-            adjacent_list_ptr vp{new vector<K>{}};
-            adjacent_lists[key] = vp;
-        }
-    }
-    void add_node(const K &key) {
-        nodes[key] = unique_ptr<attr>{new attr{}};
-        init_adjlist(key);
-    }
+    void init_adjlist(const K &key);
+    void add_node(const K &key);
     void add_node(const K &key, const attr &a);
     void set_node_attr(const K &key, const attr &a);
-    void add_node_attr(const K &key, const string &attr_name, boost::any &value);
+    void add_node_attr(const K &key, const string &attr_name,
+                       boost::any &value);
     boost::any get_node_attr(const K &key, const string &attr_name);
     void print_graph();
     void add_edge(const K &from, const K &to);
     void add_edge_attr(const K &from, const K &to, const string &key,
                        boost::any &value);
-    boost::any get_edge_attr(const K &from, const K&to, const string &key);
-    void set_edge_weight(const K&from, const K &to, int weight);
-    int get_edge_weight(const K&from, const K &to);
+    boost::any get_edge_attr(const K &from, const K &to, const string &key);
+    void set_edge_weight(const K &from, const K &to, int weight);
+    int get_edge_weight(const K &from, const K &to);
     adjacent_list_ptr get_neighbors_by_node(const K &);
 };
 
-template<class K>
-void Graph<K>::set_edge_weight(const K&from, const K &to, int weight){
+template <class K>
+void Graph<K>::init_adjlist(const K &key) {
+    if (!exist_in_map(key, adjacent_lists)) {
+        adjacent_list_ptr vp{new vector<K>{}};
+        adjacent_lists[key] = vp;
+    }
+}
+
+template <class K>
+void Graph<K>::add_node(const K &key) {
+    nodes[key] = unique_ptr<attr>{new attr{}};
+    init_adjlist(key);
+}
+
+template <class K>
+void Graph<K>::set_edge_weight(const K &from, const K &to, int weight) {
     auto w = boost::any{weight};
     add_edge_attr(from, to, "weight", w);
 }
 
-template<class K>
-int Graph<K>::get_edge_weight(const K&from, const K &to){
+template <class K>
+int Graph<K>::get_edge_weight(const K &from, const K &to) {
     return boost::any_cast<int>(get_edge_attr(from, to, "weight"));
 }
 
 template <class K>
-boost::any Graph<K>::get_edge_attr(const K&from, const K&to, const string &key){
-   return (*edge_attrs[make_pair(from, to)])[key];
+boost::any Graph<K>::get_edge_attr(const K &from, const K &to,
+                                   const string &key) {
+    return (*edge_attrs[make_pair(from, to)])[key];
 }
 
 template <class K>
 void Graph<K>::add_node_attr(const K &key, const string &attr_name,
                              boost::any &value) {
-    nodes[key][attr_name] = value;
+    (*nodes[key])[attr_name] = value;
 }
 
 template <class K>
 boost::any Graph<K>::get_node_attr(const K &key, const string &attr_name) {
-    return nodes[key][attr_name];
+    return (*nodes[key])[attr_name];
 }
 
 template <class K>
@@ -96,29 +104,25 @@ void Graph<K>::add_node(const K &key, const attr &a) {
 
 template <class K>
 void Graph<K>::set_node_attr(const K &key, const attr &a) {
-    nodes[key] = a;
+    unique_ptr<attr> p{new attr{a}};
+    nodes[key] = move(p);
 }
 
 template <class K>
 void Graph<K>::print_graph() {
-    for (auto &x : nodes) {
-        cout << x.first << endl;
-    }
     for (auto &x : adjacent_lists) {
-        cout << x.first << " : ";
         for (auto &e : *x.second) {
-            cout << e << " ";
+            cout << x.first << " -> " << e << endl;
         }
-        cout << endl;
     }
 }
 
 template <class K>
 void Graph<K>::add_edge(const K &from, const K &to) {
     if (!exist_in_map(from, nodes)) {
-        throw runtime_error("node does not exist!");
+        add_node(from);
     } else if (!exist_in_map(to, nodes)) {
-        throw runtime_error("node does not exist!");
+        add_node(from);
     }
     pair<K, K> np{from, to};
     if (exist_in_map(np, edge_attrs)) {
@@ -128,8 +132,8 @@ void Graph<K>::add_edge(const K &from, const K &to) {
     edge_attrs[np] = unique_ptr<attr>{new attr{}};
 }
 template <class K>
-void Graph<K>::add_edge_attr(const K &from, const K &to, const string &attr_name,
-                             boost::any &value) {
+void Graph<K>::add_edge_attr(const K &from, const K &to,
+                             const string &attr_name, boost::any &value) {
     pair<K, K> np{from, to};
     (*edge_attrs[np])[attr_name] = value;
 }
--- test.cc
+++ test.cc
@@ -1,47 +1,44 @@
-#include "graph.cc"
-
 #include <cassert>
+#include <string>
+
+#include "graph.cc"
 // Use (void) to silent unused warnings.
 #define assertm(exp, msg) assert(((void)msg, exp))
 
-int test_add_node(){
-  Graph<string> g;
-  g.add_node("a");
-  g.add_node("b");
-  g.add_node("c");
-  g.add_edge("a", "b");
-  g.add_edge("b", "c");
-  try{
+void test_add_node() {
+    Graph<string> g;
+    g.add_node("a");
+    g.add_node("b");
+    g.add_node("c");
+    g.add_edge("a", "b");
+    g.add_edge("b", "c");
     g.add_edge("k", "c");
-  }catch (std::runtime_error &e){
-      return 0;
-  }
-  assertm(false, "add non existing node should raise");
-}
-int test_add_edge(){
-  Graph<string> g;
-  g.add_node("a");
-  g.add_node("c");
-  g.add_edge("a", "c");
-  try {
     g.add_edge("a", "c");
+    g.add_edge("a", "m");
+    g.print_graph();
+}
+int test_add_edge() {
+    Graph<string> g;
+    g.add_node("a");
+    g.add_node("c");
+    g.add_edge("a", "c");
+    try {
+        g.add_edge("a", "c");
 
-  }catch(std::runtime_error &e) {
-    return 0;
-  }
-  assertm(false, "add existing edge should raise");
-
+    } catch (std::runtime_error &e) {
+        return 0;
+    }
+    assertm(false, "add existing edge should raise");
 }
 
-int test_add_weight(){
-  Graph<string> g;
-  g.add_node("a");
-  g.add_node("c");
-  g.add_edge("a", "c");
-  g.set_edge_weight("a", "c", 1456);
-  assertm(g.get_edge_weight("a", "c") == 1456, "test set weight error");
-  cout <<"cool"<<endl;
-  return 0;
+int test_add_weight() {
+    Graph<string> g;
+    g.add_node("a");
+    g.add_node("c");
+    g.add_edge("a", "c");
+    g.set_edge_weight("a", "c", 1456);
+    assertm(g.get_edge_weight("a", "c") == 1456, "test set weight error");
+    return 0;
 }
 
 int main() {
